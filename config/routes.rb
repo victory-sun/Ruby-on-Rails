393fb2b4675cdb9750fe66385f35dc2f7fbd874d
victory-sun <victoryriming0206@protonmail.com>
2021-07-04 15:29:13 -0400

    basic controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#index'
+
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root 'users#index'
+  
 end
